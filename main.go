447616b6d590d0f58b06653193b8aa5a64d33570
danilo lopes <105066526+danzok@users.noreply.github.com>
2023-11-09 20:54:19 -0300

    add storedb for session in device

--- main.go
+++ main.go
@@ -3,12 +3,37 @@ package main
 import (
 	"log"
 
+	waSocket "github.com/amiruldev20/waSocket"
+	"github.com/amiruldev20/waSocket/store/sqlstore"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	//load yours variable in .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	//create you db sqlite3
+	container, err := sqlstore.New("sqlite3", "file:yoursqlitefile.db?_foreign_keys=on", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// save the first device session
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sock := waSocket.NewClient(deviceStore, nil)
+
+	//	eventHandler := registerHandler(sock)
 }
+
+/*
+func registerHandler(sock *waSocket.Client) {
+
+}*/
